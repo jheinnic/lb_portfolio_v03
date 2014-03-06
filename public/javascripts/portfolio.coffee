19916f8f1d85b9263f88b8b45f71a6e9e1c55ff3
John Heinnickel <jheinnic@hotmail.com>
2014-03-05 16:06:20 -0800

    Added navigation bar prototype."

--- public/javascripts/portfolio.coffee
+++ public/javascripts/portfolio.coffee
@@ -1,8 +1,8 @@
-window.App = angular.module 'jch-portfolio', ['ngResource', 'ngRoute']
+window.App = angular.module 'jch-portfolio', ['ngResource', 'ngRoute', 'ngCookies', 'ui.bootstrap', 'mgcrea.ngStrap']
 
-app = window.App
+appModule = window.App
 
-app.controller 'NavBarCtrl', ['$scope', '$location', ($scope, $location) ->
+appModule.controller 'NavBarCtrl', ['$scope', '$location', ($scope, $location) ->
     $scope.section = 'home';
   
     paths =
@@ -16,17 +16,52 @@ app.controller 'NavBarCtrl', ['$scope', '$location', ($scope, $location) ->
       $location.path paths[newSection] 
 ]
 
-app.controller 'HomeCtrl', [
+appModule.controller 'HomeCtrl', [
   '$scope',
   ($scope) ->
     $scope.message = 'Hello world!'
 ]
 
-app.config ['$routeProvider', ($routeProvider) ->
+appModule.config ['$routeProvider', ($routeProvider) ->
   $routeProvider.when '/', 
-    templateUrl: 'partials/portfolio/view.html',
+    templateUrl: 'public/partials/portfolio/view.html',
     controller: 'HomeCtrl'
   $routeProvider.when '/crosswords', 
-    templateUrl: 'partials/crosswords/view.html'
+    templateUrl: 'public/partials/crosswords/view.html'
     controller: 'CrosswordsCtrl'
+]
+appModule.directive 'navBarPlacement', [
+  'navBarDataSvc',
+  (jchNbSvc) ->
+    restrict: 'E',
+    templateUrl: 'public/partials/portfolio/navBar.html',
+    link: (scope, element, attrs) ->
+      updateModel = () ->
+        scope.nbDataModel = jchNbSvc.getDataModel();
+        scope.nbDataModel.refreshPromise.then( updateModel )
+      updateModel()
+]
+
+appModule.factory 'navBarDataSvc', [
+  '$q',
+  ($q) ->
+    updateHandle = $q.defer()
+    nbDataModel =
+      brandName: 'John Heinnickel',
+      tabModels: [
+        {matchRoute: '/'
+        clickRoute: '/'
+        displayLabel: 'Home' }
+        { clickRoute: '/crosswords'
+        displayLabel: 'Crosswords' }
+        { matchRoute: '/videos'
+        clickRoute: '/nowhere'
+        displayLabel: 'Videos' }
+        { matchRoute: '/poker'
+        clickRoute: '/.*'
+        displayLabel: 'Poker' }
+      ],
+      refreshPromise: updateHandle.promise
+
+    return { getDataModel: () -> return nbDataModel }
 ]
--- server.js
+++ server.js
@@ -15,75 +15,86 @@ var EmployeeProvider = require('./lib/employeeprovider').EmployeeProvider;
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', relPath('views'));
-app.set('view engine', 'jade');
-app.locals.basedir = relPath('views');
-
-// What do these do?
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-
-// "Boilerplate you should already have"?
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-
-// Compile .less and .coffee files, then map resources under /public to static files relative to __dirname/public.
-app.use(lessMiddleware({src:relPath('public'), force:true, debug:true}));
-app.use(coffeeMiddleware({src:relPath('public'), force:true, debug:true}));
-app.use('/public', express.static(relPath('public')));
-
-// Establish a static path root for bower component dependencies.
-app.use('/components', express.static(relPath('/components')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+/**
+ * Utility shorthand for appending one element to _dirName
+ * @param subPath
+ * @returns {*|string}
+ */
+function relPath(subPath) {
+    return path.join(__dirname, subPath);
 }
 
+app.configure( function() {
+    // all environments
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', relPath('views'));
+    app.set('view engine', 'jade');
+    app.locals.basedir = relPath('views');
 
-var employeeDAO = new EmployeeProvider('localhost', 27017);
+    // What do these do?
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded());
 
-//Routes
+    // "Boilerplate you should already have"?
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
 
-app.get('/employees', function(req, res){
-  employeeDAO.findAll(function(error, emps){
-      res.render('index', {
-            title: 'Employees',
-            employees:emps
+    // Compile .less and .coffee files, then map resources under /public to static files relative to __dirname/public.
+    app.use('/public', lessMiddleware({src:relPath('public'), paths:[path.join(__dirname, 'components', 'bootstrap', 'less')], prefix:'/public', force:true, debug:true}));
+    app.use('/public', coffeeMiddleware({src:relPath('public'), prefix:'/public', force:true, debug:true}));
+    app.use('/public', express.static(relPath('public')));
+
+    // Establish a static path root for bower component dependencies.
+    // app.use('/components', lessMiddleware({src:relPath('components'), prefix:'/components', force:true, debug:true}));
+    app.use('/components', express.static(relPath('/components')));
+
+    // Map URIs that begin with /img to Bootstrap's img directory.
+    app.use('/img', express['static'](path.join(__dirname, 'components', 'bootstrap', 'img')));
+
+    // development only
+    if ('development' == app.get('env')) {
+      app.use(express.errorHandler());
+    }
+
+
+    var employeeDAO = new EmployeeProvider('localhost', 27017);
+
+    //Routes
+
+    app.get('/employees', function(req, res){
+      employeeDAO.findAll(function(error, emps){
+          res.render('index', {
+                title: 'Employees',
+                employees:emps
+            });
+      });
+    });
+
+    app.get('/employee/new', function(req, res) {
+        res.render('employee_new', {
+            title: 'New Employee',
+            cause: 'Just Be'
         });
-  });
-});
-
-app.get('/employee/new', function(req, res) {
-    res.render('employee_new', {
-        title: 'New Employee',
-        cause: 'Just Be'
     });
-});
 
-//save new employee
-app.post('/employee/new', function(req, res){
-    employeeDAO.save({
-        title: req.param('title'),
-        name: req.param('name')
-    }, function( error, docs) {
-        res.redirect('/');
+    //save new employee
+    app.post('/employee/new', function(req, res){
+        employeeDAO.save({
+            title: req.param('title'),
+            name: req.param('name')
+        }, function( error, docs) {
+            res.redirect('/');
+        });
     });
-});
 
-app.get('/', routes.index);
-app.get('/partials/*', routes.partials);
-// app.get('/users', user.list);
+    app.get('/', routes.index);
+    app.get('/partials/*', routes.partials);
+    // app.get('/users', user.list);
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
-function relPath(subPath) {
-    return path.join(__dirname, subPath);
-}
