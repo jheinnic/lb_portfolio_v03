6be357560b1cc3b6b3b07ffac3a53ee062418453
John Heinnickel <jheinnic@hotmail.com>
2015-12-09 13:27:37 -0800

    Patched 07-explorer boot script to accomodate breaking changes in the recently adopted minor revision update to loopback-component-explorer.

--- server/boot/07-explorer.js
+++ server/boot/07-explorer.js
@@ -3,12 +3,9 @@ var explorer = require('loopback-explorer');
 module.exports = function(server) {
   var restApiRoot = server.get('restApiRoot');
 
-  var explorerApp = explorer(server, { basePath: restApiRoot });
-  console.log(restApiRoot);
-  console.log(explorerApp);
-  server.use('/explorer', explorerApp);
+  var explorerApp = explorer(server, { mountPath: '/explorer', basePath: restApiRoot });
   server.once('started', function() {
     var baseUrl = server.get('url').replace(/\/$/, '');
-    console.log('Browse your REST API at %s%s', baseUrl, explorerApp.mountpath);
+    console.log('Browse your REST API at %s%s', baseUrl, '/explorer');
   });
 };
