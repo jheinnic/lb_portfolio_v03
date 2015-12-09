6e98756e84fc5eece1602ed9613ca79cc9b29da0
John Heinnickel <jheinnic@hotmail.com>
2015-12-09 13:26:48 -0800

    Removed traces of deprecated loopback-jsonschema plugin tha thad not worked out as anticipated.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
 var path = require('path');
-var loopbackJsonSchema = require('loopback-jsonschema');
 
 var app = module.exports = loopback();
 
@@ -20,10 +19,6 @@ if (livereload) {
 
 // boot scripts mount components like REST API
 boot(app, __dirname);
-
-// Enable JSON Schema support
-loopbackJsonSchema.init(app);
-loopbackJsonSchema.enableJsonSchemaMiddleware(app);
 
 // Mount static files like ngapp
 // All static middleware should be registered at the end, as all requests
