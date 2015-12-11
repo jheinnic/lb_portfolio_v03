053d07fb1150ac220660b5444d02fe2f124ba962
John Heinnickel <jheinnic@hotmail.com>
2015-12-10 22:18:22 -0800

    Removing unnecessary commented out code

--- grunt/jade.js
+++ grunt/jade.js
@@ -1,36 +1,7 @@
 'use strict';
 
-var pkg = require('./utils/pkg');
 module.exports = function jade(grunt, options) {
-  //function assetMethod(assetType, selfModule, moduleName, assetPath) {
-  //  if (assetPath === undefined) {
-  //    assetPath = moduleName;
-  //    moduleName = selfModule;
-  //  }
-  //
-  //  return assetType + '/' + moduleName + '/' + assetPath;
-  //}
-
   return {
-    //play: {
-    //  options: {
-    //    data: function (dest, src) {
-    //      // TODO: Extract the module path from src?
-    //      console.log('Src(', src, ') -> Dest(', dest, ')');
-    //      var selfModule = 'myself';
-    //
-    //      return {
-    //        viewAsset: _.partial(assetMethod, 'views', selfModule),
-    //        imageAsset: _.partial(assetMethod, 'images', selfModule),
-    //        styleAsset: _.partial(assetMethod, 'styles', selfModule),
-    //        fontAsset: _.partial(assetMethod, 'fonts', selfModule)
-    //      };
-    //    }
-    //  },
-    //  files: {
-    //    '<%= appConfig.dev %>/play.html': '<%= appConfig.app %>/play.jade'
-    //  }
-    //},
     dev: {
       options: {
         data: { isDev: true }
