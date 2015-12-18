10fe9a1cbe2307d2e7979834be9ecae0cc15bae6
John Heinnickel <jheinnic@hotmail.com>
2015-12-18 02:49:11 -0800

    Migrating more from junk to refJunk

--- server/boot/70-angular-routes.js
+++ server/boot/70-angular-routes.js
@@ -2,26 +2,21 @@
   'use strict';
 
   module.exports = function (app) {
-     /*
-     var routes = require('../../client/ngapp/config/routes');
-     function routePathToIndexFile(route) {
-       app.get(
-         route,
-         function(req, res) {
-           app.loopback.static(
-             app.get('indexFile')
-           );
-         }
-       )
-     }
-    Object.keys(routes).forEach(routePathToIndexFile);
+    /*
+    var routes = require('../../client/ngapp/config/routes');
+    Object.keys(routes).forEach(
+      function(route) {
+        app.get(route, function(req, res) {
+        res.sendfile(app.get('indexFile'));
+      }
+    );
     */
 
     // Until there is a way to target specific paths,
     app.use(
-      '/**', app.loopback.static(
+      '/**', loopback.static(
         app.get('indexFile')
       )
     );
-   };
+  };
 }).call();
