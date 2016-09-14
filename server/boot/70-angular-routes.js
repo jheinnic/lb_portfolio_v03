27c118f196376c66c7667d0b46160aacf8f57e41
John Heinnickel` <jheinnic@gmail.com>
2016-09-14 13:21:17 -0700

    COrrect error in angular static routing config

--- server/boot/70-angular-routes.js
+++ server/boot/70-angular-routes.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  module.exports = function (app) {
+  module.exports = function (server) {
     /*
     var routes = require('../../client/ngapp/config/routes');
     */
@@ -9,16 +9,26 @@
     var process = require('process');
     var indexFile =
       path.normalize(
-        path.join(process.cwd(), app.get('indexFile')));
+        path.join(process.cwd(), server.get('indexFile')));
     console.log('** Index file is at ' + indexFile);
 
     function sendIndexFile(req, res) {
       res.sendFile(indexFile);
     }
 
+    // Add a status page under /status
+    var router = server.loopback.Router();
+    router.get('/status', server.loopback.status());
+
     // Until there is a way to target specific paths,
-    app.get('/', sendIndexFile);
-    app.get('/**', sendIndexFile);
-    app.get('/index.html', sendIndexFile);
+    router.get('/', sendIndexFile);
+    router.get('/index', sendIndexFile);
+    router.get('/index.htm', sendIndexFile);
+    router.get('/index.html', sendIndexFile);
+
+    // This wildcard must follow after all NGApp path have explicit routing!
+    router.get('/**', sendIndexFile);
+
+    server.use(router);
   };
 }).call();
