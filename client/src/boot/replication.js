8097ad698f4d37699ea963919d45618b4db114ab
John Heinnickel` <jheinnic@gmail.com>
2016-09-14 13:12:19 -0700

    Update to current Loopback client replication script content requirements.

--- client/src/boot/replication.js
+++ client/src/boot/replication.js
@@ -8,12 +8,16 @@
 //    replicate: {
 //      target: 'Todo',
 //      mode: 'push' | 'pull' | 'bidi'
-//    }}}
-module.exports = function(client) {
-  var LocalTodo = client.models.LocalTodo;
-  var RemoteTodo = client.models.Todo;
+// Copyright IBM Corp. 2015. All Rights Reserved.
+// Node module: loopback-example-isomorphic
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
 
-  client.network = {
+module.exports = function(app) {
+  var LocalTodo = app.models.LocalTodo;
+  var RemoteTodo = app.models.RemoteTodo;
+
+  app.network = {
     _isConnected: true,
     get isConnected() {
       console.log('isConnected?', this._isConnected);
@@ -24,18 +28,45 @@ module.exports = function(client) {
     }
   };
 
-  // setup model replication
+  var since = {push: -1, pull: -1};
   function sync(cb) {
-    if (client.network.isConnected) {
-      RemoteTodo.replicate(LocalTodo, function() {
-        LocalTodo.replicate(RemoteTodo, cb);
+    console.log('sync triggered');
+    LocalTodo.replicate(
+      RemoteTodo,
+      since.push,
+      function pushed(err, conflicts, cps) {
+        console.log('LR', arguments);
+        since.push = cps;
+        RemoteTodo.replicate(
+          LocalTodo,
+          since.pull,
+          function pulled(err, conflicts, cps) {
+            console.log('RL', arguments);
+            since.pull = cps;
+          });
       });
-    }
   }
 
   // sync local changes if connected
-  LocalTodo.on('changed', sync);
-  LocalTodo.on('deleted', sync);
+  function legacyChangedObserver(arg) {
+    console.log('Fired on changed');
+  });
+  LocalTodo.on('changed', legacyChangedObserver);
+  function legacyDeletedObserver(arg) {
+    if (arg )
+    console.log('Fired on changed');
+  });
+  LocalTodo.on('deleted', legacyDeletedObserver);
 
-  client.sync = sync;
+  LocalTodo.observe('after save', function(ctx, next) {
+    next();
+    sync();
+  });
+
+  LocalTodo.observe('after delete', function(ctx, next) {
+    next();
+    sync();
+  });
+
+  app.sync = sync;
 };
